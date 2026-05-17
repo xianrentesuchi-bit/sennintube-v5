eb4544d258e27cb7a877fe4abc2545adf270cb35
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-17 17:11:45 +0900

    Update suggests.js

--- routes/suggests.js
+++ routes/suggests.js
@@ -5,9 +5,19 @@ const axios = require('axios');
 router.get('/', async (req, res) => {
     const query = req.query.q;
     try {
-        const response = await axios.get(`https://suggestqueries.google.com/complete/search?client=firefox&hl=ja&q=${encodeURIComponent(query)}`);
-        res.json(response.data[1]);
+        const response = await axios.get(`https://suggestqueries.google.com/complete/search?client=firefox&hl=ja&q=${encodeURIComponent(query)}`, {
+            responseType: 'arraybuffer' // バイナリデータとして受け取る
+        });
+        
+        // UTF-8文字列にデコードする
+        const decoder = new TextDecoder('utf-8');
+        const decodedData = JSON.parse(decoder.decode(response.data));
+        
+        // レスポンスヘッダーに明示的に UTF-8 を指定
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.json(decodedData[1]);
     } catch (e) {
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
         res.json([]);
     }
 });
