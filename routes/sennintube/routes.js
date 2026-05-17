f8fde8fddeb5aef9ba40a470e71a954e22cfe402
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-18 06:48:42 +0900

    Update routes.js

--- routes/sennintube/routes.js
+++ routes/sennintube/routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const stickmanRouter = require('./sennintube/game/server/stickman.js');
+
 router.get('/help', (req, res) => {
     res.render('tool/help');
 });
 
 router.get('/login', (req, res) => {
     res.render('taisaku/login');
+});
+
+router.use('/game/stickman', stickmanRouter);
+
+router.get('/gamemanu', (req, res) => {
+    res.render('tool/game');
 });
 
 module.exports = router;
