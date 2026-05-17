d04b97319c8a71bdfa8ff7cf93fc104e5a38b5e3
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-17 20:17:14 +0900

    Update channel.js

--- routes/channel.js
+++ routes/channel.js
@@ -4,7 +4,10 @@ const getChannelInfo = require('../api/youtube/channelinfo');
 
 router.get('/:id', async (req, res) => {
     const channelId = req.params.id;
-    const channelInfo = await getChannelInfo(channelId);
+    const sortBy = req.query.sort_by || 'newest';
+    const forceInstance = req.query.force_instance || null;
+
+    const channelInfo = await getChannelInfo(channelId, sortBy, forceInstance);
     if (!channelInfo) return res.status(404).send('Channel Not Found');
     res.render('channel', { channelInfo });
 });
